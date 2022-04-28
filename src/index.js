af28ddd5857e3c7a5a3c1c231ac4e103568762f1
Eden Castillo Asport <35803612+phenomenone@users.noreply.github.com>
2022-04-28 16:49:53 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My Favourite Foods</h1>
+  </div>,
+  document.getElementById("root")
+);
 //Create a react app from scratch.
 //It should display a h1 heading.
 //It should display an unordered list (bullet points).
