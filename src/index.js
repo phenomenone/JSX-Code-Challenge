d8bad0c289af16e100cd929fae8ec50e50f2846e
Eden Castillo Asport <35803612+phenomenone@users.noreply.github.com>
2022-04-28 16:51:15 -0500

    complete challenge

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import ReactDOM from "react-dom";
 ReactDOM.render(
   <div>
     <h1>My Favourite Foods</h1>
+    <ul>
+      <li>Bacon</li>
+      <li>Jamon</li>
+      <li>Noodles</li>
+    </ul>
   </div>,
   document.getElementById("root")
 );
